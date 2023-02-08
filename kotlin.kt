4fce6d544af1db27b3a044d2a9bb74a1c03f558a
Darian-Cătălin Cucer <86965240+cucerdariancatalin@users.noreply.github.com>
2023-02-08 11:21:55 +0200

    Create kotlin.kt

---
+++ kotlin.kt
@@ -0,0 +1,42 @@
+fun kmp(text: String, pattern: String): Int {
+    val lps = computeLPSArray(pattern)
+    var i = 0
+    var j = 0
+    while (i < text.length) {
+        if (pattern[j] == text[i]) {
+            j++
+            i++
+        }
+        if (j == pattern.length) {
+            return i - j
+        } else if (i < text.length && pattern[j] != text[i]) {
+            if (j != 0) {
+                j = lps[j - 1]
+            } else {
+                i++
+            }
+        }
+    }
+    return -1
+}
+
+fun computeLPSArray(pattern: String): IntArray {
+    val lps = IntArray(pattern.length)
+    var len = 0
+    var i = 1
+    lps[0] = 0
+    while (i < pattern.length) {
+        if (pattern[i] == pattern[len]) {
+            len++
+            lps[i] = len
+            i++
+        } else {
+            if (len != 0) {
+                len = lps[len - 1]
+            } else {
+                lps[i] = len
+                i++
+            }
+        }
+    }
+    return lps
